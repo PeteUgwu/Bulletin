5f1337f3b50bf8883b1b312bc885a2a57791f382
peter <peterugwu64@gmail.com>
2023-06-06 22:23:32 +0100

    Update mobile responsiveness

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    'https://newsapi.org/v2/everything?q=tesla&from=2023-05-01&sortBy=publishedAt&apiKey=7d6c2de756014f95822c31c2a8de94a0',
+    "https://newsapi.org/v2/everything?q=tesla&from=2023-05-01&sortBy=publishedAt&apiKey=7d6c2de756014f95822c31c2a8de94a0",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
